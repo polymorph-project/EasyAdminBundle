eed562ebf3d85f105486b1df6f09b7bb9adf4030
Mbechezi Mlanawo <mlanawo.mbechezi@kemeter.io>
2021-12-07 23:28:15 +0100

    Bump Symfony version to 2.4.0

--- src/EasyAdminBundle.php
+++ src/EasyAdminBundle.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class EasyAdminBundle extends Bundle
 {
-    public const VERSION = '2.3.16-DEV';
+    public const VERSION = '2.4.0-DEV';
 
     public function build(ContainerBuilder $container)
     {
